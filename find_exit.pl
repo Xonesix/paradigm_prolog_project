d107484bd9935c04230facda8aa74188cf7bc569
Xonesix <arjuntx@gmail.com>
2025-05-10 13:07:43 -0500

    revelation

---
+++ find_exit.pl
@@ -0,0 +1,23 @@
+/*
+Ok we need coordinates
+
+so i guess it would be like coord(X,Y) something like that
+
+then maybe a move function.
+That will move the coordinates, if it's not a wall etc. etc.
+
+
+
+*/
+
+
+/*
+The coordinates must start at the start coordinate
+So find a value that has [s] in it i guess
+
+WAIT A MINUTE THIS IS JSUT DFS IN PROLOG
+
+IF I CAN IMPLEMENT DFS INTO PROLOG AND STOP WHEN I FIND THE EXIT
+AND BACKTRACK THE COORDINATES I CAN SOLVE THE MAZE
+i need to figure out how to do DFS
+*/
